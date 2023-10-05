0bd965357625d54365c6e88e337841562df5c4a3
fynoeb <104561031+fynoeb@users.noreply.github.com>
2023-10-05 19:19:34 +0700

    commit tugas transaksi
    Signed-off-by: fynoeb <104561031+fynoeb@users.noreply.github.com>

---
+++ praktikumAlpro2311537001/src/pekan2/TotalHargaBayar.java
@@ -0,0 +1,17 @@
+package Pekan2;
+
+public class TotalHargaBayar {
+
+	public static void main(String[] args) {
+		double hargabarang=220500;
+		double jumlahbarang=7;
+		double diskon=0.3;
+		double totalharga;
+		
+		
+		totalharga= (hargabarang*jumlahbarang)*(1-diskon);
+		System.out.println("total harga bayar = "+ totalharga);
+		
+  }
+
+}
