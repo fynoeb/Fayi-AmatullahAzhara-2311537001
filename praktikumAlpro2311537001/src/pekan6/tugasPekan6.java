ea24d2c750beebaeeb558c73621467be6e4e9962
fayiamatullahazhara <fayiamatullahazhara@10.5.50.23>
2023-11-25 17:49:34 +0700

    tugas pekan 6

---
+++ praktikumAlpro2311537001/src/pekan6/tugasPekan6.java
@@ -0,0 +1,24 @@
+package pekan6;
+import java.util.Scanner;
+
+public class tugasPekan6 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+        System.out.println("Inputkan Angka : ");
+        double n = in.nextInt();
+        
+        for (int i=1; i<=n; i++) {
+        	if (i%2 == 1 ) {
+        		for (int j=1; j<=Math.ceil(n/2); j++) {
+        			 System.out.print("-");
+        		}
+        	} else {
+        		for (int j=1; j<=Math.floor(n/2); j++) {
+       			 System.out.print("-");
+        	}
+        		
+        }
+        	System.out.println();
+	}
+}
+}
